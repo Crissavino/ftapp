1636093210bff2bd2c091e040631512aca8b6283
cristiansavino <savinocristian89@gmail.com>
2021-01-21 17:12:37 +0000

    delete env

--- lib/utils/environment.dart
+++
@@ -1,7 +0,0 @@
-class Environment {
-  // static String apiUrl = 'http://localhost:3000/api';
-  // static String socketUrl = 'http://localhost:3000';
-  static String apiUrl = 'https://880422ef6e11.eu.ngrok.io/api';
-  static String socketUrl = 'https://880422ef6e11.eu.ngrok.io';
-  static String googlePlaceApiKey = 'AIzaSyCIW7l5SSIkF7JS1LOQAi6Rcsmm_10yJTQ';
-}
